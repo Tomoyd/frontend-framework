8e3e879b23c25b56461fd5d1ef45b8aeeb7c3aac
tomoyd <ydh9419@163.com>
2022-04-10 22:28:38 +0800

    -mcommoit

--- react-proj/src/App.js
+++ react-proj/src/App.js
@@ -1,12 +1,28 @@
-import "./App.css";
+import { useEffect, useLayoutEffect, useState } from 'react';
+import './App.css';
 import {
   InheritanceInversionComponent,
   PropProxyComponent,
-} from "./demos/higher-component";
+} from './demos/higher-component';
 
 function App() {
+  const [count, setCount] = useState(0);
+  useLayoutEffect(() => {
+    console.log('useLayout');
+    return () => {
+      console.log('useLayout>>clean', 1234);
+    };
+  });
+  useEffect(() => {
+    console.log('useEffect');
+    return () => {
+      console.log('useEffect>>clean');
+    };
+  }, []);
   return (
     <div className='App'>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>add</button>
       <header className='App-header'>
         <PropProxyComponent />
         <InheritanceInversionComponent></InheritanceInversionComponent>
