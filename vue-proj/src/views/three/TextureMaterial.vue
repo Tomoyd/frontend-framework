433ce1203f28c9dd5335e5f8eb377fb911819365
tomoyd <ydh9419@163.com>
2022-08-21 13:31:51 +0800

    chore: texture  代码优化

--- vue-proj/src/views/three/TextureMaterial.vue
+++ vue-proj/src/views/three/TextureMaterial.vue
@@ -27,16 +27,14 @@ import {
   BoxGeometry,
   BufferAttribute,
   BufferGeometry,
-  BufferGeometryLoader,
-  CubeCamera,
   DoubleSide,
   Group,
   IcosahedronGeometry,
   Material,
   Mesh,
+  MeshLambertMaterial,
   MeshPhongMaterial,
   MeshStandardMaterial,
-  PMREMGenerator,
   SphereGeometry,
   Texture,
   Vector2,
@@ -55,18 +53,117 @@ function getTextureType() {
 }
 
 function getUVMap() {
-  return {
-    bumpMap: 'stone-bump.jpg',
-    normalMap: 'plaster-normal.jpg',
-    displacementMap: 'w_d.png',
-    lightMap: 'lightmap.png',
-    aoMap: 'ambient.png',
-    roughnessMap: 'roughness-map.jpg',
-    alphaMap: 'alpha.png',
-    emissiveMap: 'lava.png',
-    specularMap: '',
-    //  emissiveMap  specularMap ,envMap
+  const mapFn = {
+    async specularMap() {
+      const [map, normalMap, specularMap] = await loadTexture(
+        ['Earth.png', 'EarthNormal.png', 'EarthSpec.png'],
+        '/three/textures/earth/'
+      );
+      (box.material as Material) = new MeshPhongMaterial({
+        map,
+        normalMap,
+        specularMap,
+        normalScale: new Vector2(6, 6),
+        shininess: 10,
+        specular: 0xffff00,
+      });
+    },
+    async emissiveMap() {
+      const [emissiveMap, normalMap, metalnessMap] = await loadTexture(
+        ['lava.png', 'lava-normals.png', 'lava-smoothness.png'],
+        '/three/textures/emissive/'
+      );
+      box.material.emissiveMap = emissiveMap;
+      box.material.normalMap = normalMap;
+      box.material.metalnessMap = metalnessMap;
+      box.material.metalness = 1;
+      box.material.roughness = 0.05;
+    },
+    async roughnessMap() {
+      const [map] = await loadTexture(
+        ['roughness-map.jpg'],
+        '/three/textures/'
+      );
+
+      box.material.roughnessMap = map;
+
+      box.material.metalness = 1;
+      box.material.roughness = 0.4;
+    },
+    async metalnessMap() {
+      const [map] = await loadTexture(
+        ['roughness-map.jpg'],
+        '/three/textures/'
+      );
+
+      box.material.metalnessMap = map;
+
+      box.material.metalness = 1;
+      box.material.roughness = 0.04;
+    },
+    async envMap() {
+      const { cubeCamera } = createCubeCamera();
+      // box.material.envMap = createCubeTexture('car');
+      box.material.envMap = cubeCamera.renderTarget.texture;
+      cubeCamera.position.copy(box.position);
+      box.material.metalness = 1;
+      box.material.roughness = 0.01;
+      cubeCamera.update(three.getRenderer(), three.getScene());
+    },
+    async displacementMap() {
+      const [map, displacementMap] = await loadTexture(
+        ['w_c.jpg', 'w_d.png'],
+        '/three/textures/'
+      );
+      box.material.map = map;
+      box.material.displacementMap = displacementMap;
+    },
+    async bumpMap() {
+      const [map, bumpMap] = await loadTexture(
+        ['stone.jpg', 'stone-map.png'],
+        '/three/textures/'
+      );
+      box.material.map = map;
+      box.material.bumpMap = bumpMap;
+    },
+    async normalMap() {
+      const [map, normalMap] = await loadTexture(
+        ['plaster.jpg', 'plaster-normal.jpg'],
+        '/three/textures/'
+      );
+      box.material.map = map;
+      box.material.aoMap = normalMap;
+    },
+    async aoMap() {
+      const [aomap] = await loadTexture(['ambient.png'], '/three/textures/');
+      box.material.aoMap = aomap;
+    },
+    async lightMap() {
+      const [lightMap] = await loadTexture(
+        ['lightmap.png'],
+        '/three/textures/'
+      );
+      box.material.lightMap = lightMap;
+    },
+    async alphaMap() {
+      await this.map();
+      box.material.metalness = 0.02;
+      box.material.roughness = 0.07;
+      box.material.alphaTest = 0.5;
+      box.material.side = DoubleSide;
+    },
+    async map() {
+      box.material[
+        mapType.value as Exclude<keyof typeof mapFn, 'specularMap'>
+      ] = (
+        await loadTexture(
+          [textureOptions[textureType.value]],
+          '/three/textures/'
+        )
+      )[0];
+    },
   };
+  return mapFn;
 }
 
 function getGeometries() {
@@ -91,83 +188,6 @@ async function addObj() {
   group && three.add(group);
 }
 
-const mapFn = {
-  async specularMap() {
-    const [map, normalMap, specularMap] = await loadTexture(
-      ['Earth.png', 'EarthNormal.png', 'EarthSpec.png'],
-      '/three/textures/earth/'
-    );
-    (box.material as Material) = new MeshPhongMaterial({
-      map,
-      normalMap,
-      specularMap,
-      normalScale: new Vector2(6, 6),
-      shininess: 10,
-      specular: 0xffff00,
-    });
-  },
-  async emissiveMap() {
-    const [emissiveMap, normalMap, metalnessMap] = await loadTexture(
-      ['lava.png', 'lava-normals.png', 'lava-smoothness.png'],
-      '/three/textures/emissive/'
-    );
-    box.material.emissiveMap = emissiveMap;
-    box.material.normalMap = normalMap;
-    box.material.metalnessMap = metalnessMap;
-    box.material.metalness = 1;
-    box.material.roughness = 0.05;
-  },
-  async roughnessMap() {
-    const [map] = await loadTexture(['roughness-map.jpg'], '/three/textures/');
-    box.material.metalnessMap = map;
-    // box.material.roughnessMap = map;
-
-    box.material.metalness = 1;
-    box.material.roughness = 0.05;
-    // 物理机不好时改变为MeshPhongMaterial
-    box.material.envMap = createCubeTexture('car');
-  },
-  async bumpMap() {
-    const [map, bumpMap] = await loadTexture(
-      ['stone.jpg', 'stone-map.png'],
-      '/three/textures/'
-    );
-    box.material.map = map;
-    box.material.bumpMap = bumpMap;
-  },
-  async normalMap() {
-    const [map, normalMap] = await loadTexture(
-      ['plaster.jpg', 'plaster-normal.jpg'],
-      '/three/textures/'
-    );
-    box.material.map = map;
-    box.material.aoMap = normalMap;
-  },
-  async aoMap() {
-    const [aomap] = await loadTexture(['ambient.png'], '/three/textures/');
-    box.material.aoMap = aomap;
-  },
-  async lightMap() {
-    const [lightMap] = await loadTexture(['lightmap.png'], '/three/textures/');
-    box.material.lightMap = lightMap;
-  },
-  async alphaMap() {
-    await this.loadMap();
-    box.material.metalness = 0.02;
-    box.material.roughness = 0.07;
-    box.material.alphaTest = 0.5;
-    box.material.side = DoubleSide;
-  },
-  async loadMap() {
-    if (mapType.value === 'specularMap') {
-      return;
-    }
-    box.material[mapType.value] = (
-      await loadTexture([mapOptions[mapType.value]], '/three/textures/')
-    )[0];
-  },
-};
-
 function addCubeCamera() {
   const { cube, cubeCamera } = createCubeCamera();
   cube.position.set(-30, 20, 0);
@@ -179,17 +199,32 @@ function addCubeCamera() {
     cubeCamera.update(renderer, scene);
   });
 }
+
+async function addBox() {
+  box = createCubeByGeometry(
+    await loadStandMaterialByTexture([textureOptions.texture])
+  )(geoOptions.box);
+  box.castShadow = true;
+  box.position.set(0, 0, 20);
+  const uvs = box.geometry.getAttribute('uv') as BufferAttribute;
+  // set进行更改
+  uvs.set([0.5, 0.5, 0.5], 0);
+  three.add(box);
+}
 let box: Mesh<BufferGeometry, MeshStandardMaterial>;
 const three = useThree();
-const load = getTextureLoader('/three/textures/');
+const loadStandMaterialByTexture = getTextureLoader('/three/textures/');
 
 const [textureType, textureOptions] = useObjResManage(
   getTextureType(),
   async (current, key) => {
-    box.material = await load(
-      [current],
-      key?.includes('dds') ? 'dds' : 'texture'
-    );
+    box.material.map = (
+      await loadTexture(
+        [current],
+        '/three/textures/',
+        key?.includes('dds') ? 'dds' : 'texture'
+      )
+    )[0];
   },
   'texture'
 );
@@ -204,41 +239,22 @@ const [geoType, geoOptions] = useObjResManage(
 
 const [mapType, mapOptions] = useObjResManage(
   getUVMap(),
-  async (current, key) => {
-    (Object.keys(mapOptions) as typeof key[])
-      .filter((item) => item !== 'specularMap')
-      .forEach((k) => {
-        box.material[k as Exclude<typeof k, 'specularMap'>] = null;
-      });
-    if (!key.includes('Map')) {
-      mapFn.loadMap();
-      return;
-    }
-
-    await mapFn[key as 'alphaMap']?.();
+  async (current) => {
+    await current();
     box.material.needsUpdate = true;
   },
-  'aoMap'
+  'map'
 );
 
 async function init() {
-  box = createCubeByGeometry(await load([textureOptions.texture]))(
-    geoOptions.box
-  );
-  box.castShadow = true;
-  box.position.set(0, 0, 20);
-  const uvs = box.geometry.getAttribute('uv') as BufferAttribute;
-  // set进行更改
-  uvs.set([0.5, 0.5, 0.5], 0);
-  three.add(box);
-  console.log('box.geometry.', box.geometry.getAttribute('uv'));
   addObj();
   addCubeCamera();
+  addBox();
   three.add(new AmbientLight(0x444444));
   three.add(createGroundPlane());
   three.add(createSpotLight());
-  three.mount();
   three.setBackground(createCubeTexture('car'));
+  three.mount();
   three.loopRender();
 }
 
